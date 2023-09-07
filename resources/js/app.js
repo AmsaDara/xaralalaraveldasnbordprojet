36d5441f041d7fcf19df6e0951eaad9734981985
amsadara <thiiba95@gmail.com>
2023-09-07 00:29:18 +0000

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+require('./bootstrap');
+
+import "admin-lte/plugins/jquery/jquery";
+import "admin-lte/plugins/jquery-ui/jquery-ui"
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle";
+import "admin-lte/plugins/chart.js/Chart";
+
+import "admin-lte/plugins/sparklines/sparkline";
+// import "admin-lte/plugins/jqvmap/jquery.vmap";
+// import "admin-lte/plugins/jqvmap/maps/jquery.vmap.usa";
+import "admin-lte/plugins/jquery-knob/jquery.knob.min";
+// import "admin-lte/plugins/moment/moment-with-locales";
+import "admin-lte/plugins/daterangepicker/daterangepicker";
+// import "admin-lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4";
+import "admin-lte/plugins/summernote/summernote-bs4";
+import "admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars";
+
+import "admin-lte/dist/js/adminlte";
+import "admin-lte/dist/js/demo";
+import "admin-lte/dist/js/pages/dashboard";
+
+
+
